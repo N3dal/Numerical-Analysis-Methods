a7bfe8aece252ac57d64ac19a9c913d233c43046
N3dal <nedalxzo@gmail.com>
2022-06-02 07:01:55 +0200

    create bisection function and is not ready.

--- bisection.py
+++ bisection.py
@@ -5,3 +5,17 @@ from defaults import clear
 
 # wipe terminal.
 clear()
+
+
+def bisection(function: str, tolerance: float = 1.e-10, xl: float = 1.0, xu: float = 2.0, nmax: int = 100):
+    """"""
+
+    # initialization first.
+    iter_count = 0
+    error = 1.0
+
+    while (iter_count <= nmax and error >= tolerance):
+
+        iter_count += 1
+
+        
