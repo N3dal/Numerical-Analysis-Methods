5f7ed0f7ab4168bd4dc21b53fdaff54ae5d3cf41
N3dal <nedalxzo@gmail.com>
2022-06-03 15:45:30 +0200

    move math_eval to the defaults.py file

--- defaults.py
+++ defaults.py
@@ -17,6 +17,64 @@ def clear():
         system("cls")
 
 
+def math_eval(function: str, main_variable: str = "x"):
+    """return a math function that we can,
+    evaluate.
+
+    + => for addition.
+    - => for subtraction.
+    * => for multiplication.
+    / => for division.
+    ^ => for power.
+    -x => for negative numbers.
+
+    and you can use parentheses => '(' or ')'.
+
+    note: you can only have only one main variable.
+    note: spaces is allowed but try to avoid using them.
+
+    valid examples:
+        func(3) = 3*x+23/3^2
+        func(3.3) = 8-12*23-2 * -7
+        func(3.3) = 8-12*23-2*-7
+
+    non-valid examples:
+        func(3) = 3*xy23/234
+        func(-1) = 12y+7 and the main-variable is x.
+        func(-2) = 23&34|5!math.
+
+        so you can't use any symbols except the symbols that,
+        in ELEMENTS.
+
+
+    """
+
+    # guard conditions.
+    if not function:
+        return None
+
+    # now make sure to lower the function case.
+    function = function.lower()
+
+    # and make sure to lower main variable.
+    main_variable = main_variable.lower()
+
+    # the elements that we only want to be on our function.
+    ELEMENTS = (
+        " ", "+", "-", "^", "/", "*", "(", ")", main_variable
+    )
+
+    if not all(element in ELEMENTS or element.isdecimal()
+               for element in function):
+        # if everything not ok and our function,
+        # contain other symbols or other variables.
+        return None
+
+    function = function.replace("^", "**")
+
+    return lambda value: eval(function.replace(main_variable, str(value)))
+
+
 def approximate_error(current_approx: float, past_approx: float):
     """calculate approximate error value."""
 
