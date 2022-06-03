7d6d11e1ba750a6ff2d8d5d041b0f504e7d18496
N3dal <nedalxzo@gmail.com>
2022-06-03 11:28:58 +0200

    update bisection.py

--- bisection.py
+++ bisection.py
@@ -7,6 +7,21 @@ from defaults import clear
 clear()
 
 
+def math_eval(function: str, main_variable: str = "x"):
+    """return a value from math equation,
+    using eval and make sure that the math-equation,
+    is correct."""
+
+    # guard conditions.
+    if not function:
+        return None
+
+    # now make sure to lower the function case.
+    function = function.lower()
+
+    return lambda value: eval(function.replace(main_variable, value))
+
+
 def bisection(function: str, tolerance: float = 1.e-10, xl: float = 1.0, xu: float = 2.0, nmax: int = 100):
     """"""
 
@@ -17,5 +32,3 @@ def bisection(function: str, tolerance: float = 1.e-10, xl: float = 1.0, xu: flo
     while (iter_count <= nmax and error >= tolerance):
 
         iter_count += 1
-
-        
