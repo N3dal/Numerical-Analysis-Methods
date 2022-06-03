b87a085a359d97605894f6045abfdd80140c809e
N3dal <nedalxzo@gmail.com>
2022-06-03 12:11:41 +0200

    add error functions
    we will use those function in all numerical methods so setup them in the defaults.

--- defaults.py
+++ defaults.py
@@ -15,3 +15,21 @@ def clear():
     else:
         # for windows machines.
         system("cls")
+
+
+def approximate_error(current_approx: float, past_approx: float):
+    """calculate approximate error value."""
+
+    return abs(current_approx-past_approx)/current_approx
+
+
+def true_error(true_value: float, approx_value: float):
+    """calculate true error value."""
+
+    return abs(true_value - approx_value)
+
+
+def true_relative_error(true_value: float, approx_value: float):
+    """calculate true relative error value."""
+
+    return true_error(true_value, approx_value) / true_value
