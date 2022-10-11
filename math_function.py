11dad4b5fc9c976c476d0acea32594385be0ea20
N3dal <nedalxzo@gmail.com>
2022-10-11 19:29:03 +0200

    create MathFunction.__function_variables method

--- math_function.py
+++ math_function.py
@@ -22,5 +22,38 @@ class MathFunction:
         to create your "math-equations => function" in python;
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, math_function: str):
+
+        self.math_function = math_function
+
+        # get all the function variables;
+        self.math_function_variables = self.__function_variables()
+
+    def eval(self, variable=None):
+        """
+            evaluate the math-function with the given "variable";
+
+
+            return number;
+        """
+
+    def __function_variables(self):
+        """
+            return all the function variables;
+
+
+            return [str];
+        """
+
+        return [var for var in self.math_function if var.isalpha()]
+    
+
+
+
+fx = MathFunction("2x+3")
+
+print(fx.math_function_variables)
+print(fx.math_function)
+
+
+fx(3)
