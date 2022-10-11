b21800d1024e63f2f93b3ef21f9ce9f394928b43
N3dal <nedalxzo@gmail.com>
2022-10-11 19:18:58 +0200

    create math_function.py
    using the MathFunction we can create math function using python

---
+++ math_function.py
@@ -0,0 +1,24 @@
+"""
+
+create custom math function;
+
+so we can use those object to create math functions,
+and evaluate them;
+
+"""
+
+
+from math import (sin, cos, tan)
+from tools import Tools
+
+
+Tools.clear()
+
+
+class MathFunction:
+    """
+        to create your "math-equations => function" in python;
+    """
+
+    def __init__(self):
+        pass
