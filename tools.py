3b04db027eb0e252901921bf8356bc9305603295
N3dal <nedalxzo@gmail.com>
2022-10-12 13:45:58 +0200

    update tools

--- tools.py
+++ tools.py
@@ -3,8 +3,8 @@
 
 from os import name as OS_NAME
 from os import system
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+# import numpy as np
 
 
 class Tools:
